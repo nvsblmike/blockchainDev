4bccae85b006f6cb854035568988016b8c5c33a0
nvsblmike <soundboylesh77@gmail.com>
2023-10-05 23:55:33 +0100

    make it work

---
+++ Lesson2-solidityBasics/SimpleStorage.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+    //Basic solidity types
+    //bool isFavouriteNumber = true;
+    //uint256 favouriteNumber = 5;
+    //string favouriteNumberInText = "Five";
+    //bytes32 favNumberInIDK = "cat";
+    //address myAddress = 0x788bAFda4D147f6e9686ad763414F7651d97Eb28;
+    uint256 favouriteNumber;
+}
